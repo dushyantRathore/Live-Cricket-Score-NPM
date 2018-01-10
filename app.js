903ec48772c9b63041b80095f58b77cb29cab298
drathore <dushyant.bgs@gmail.com>
2018-01-10 22:48:05 +0530

    Updated

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+#!/usr/bin/nodejs
+
 var request = require('request');
 var colors = require('colors/safe');
 
@@ -5,8 +7,37 @@ var colors = require('colors/safe');
 
 console.log(colors.red("Live Cricket Scores"))
 
-module.exports = function () {
-	/* body... */
+// module.exports = function () {
+// 	/* body... */
+
+// 	request.post(
+// 	    'https://powerful-tor-13817.herokuapp.com/live',
+// 	    { json: { key: 'value' } },
+// 	    function (error, response, body) {
+// 	        if (!error && response.statusCode == 200) {
+// 	            var x = JSON.parse(JSON.stringify(body));
+// 	            var l = x.Matches.length;
+
+// 	            var result = "\n---------------";
+
+// 	            for(var i = 0; i < l; i++){
+// 	            	var teamA = x["Matches"][i]["Team A"];
+// 	            	var teamB = x["Matches"][i]["Team B"];
+// 	            	var status = x["Matches"][i]["Status"];
+// 	            	result = result + "\nMatch : " + (i+1) + "\n";
+// 	            	result = result + "Team A : " + teamA + "\n";
+// 	            	result = result + "Team B : " + teamB + "\n";
+// 	            	result = result + "Status : " + status + "\n";
+// 	            	result = result + "\n---------------";
+// 	            }
+
+// 	            console.log(result);
+
+// 	        }
+// 	    }
+// 	);
+
+// }
 
 	request.post(
 	    'https://powerful-tor-13817.herokuapp.com/live',
@@ -35,4 +66,3 @@ module.exports = function () {
 	    }
 	);
 
-}
